ea4a02f8d69f0592fec01426c29185ddcdb9014d
razumovskiy <kolosovp94@gmail.com>
2020-10-16 15:48:15 +0300

    number models implemented

--- BitConverter.Core/Numbers/HexadecimalNumber.cs
+++ BitConverter.Core/Numbers/HexadecimalNumber.cs
@@ -1,4 +1,5 @@
 ﻿using BitConverter.Interfaces;
+using BitConverter.Models;
 
 namespace BitConverter.Numbers
 {
@@ -7,5 +8,13 @@ namespace BitConverter.Numbers
         public int Base { get; set; }
         public string IntegerPart { get; set; }
         public string FloatPart { get; set; }
+
+        public HexadecimalNumber(string input)
+        {
+            INumber number = new NumberModel(input, 16);
+            Base = number.Base;
+            IntegerPart = number.IntegerPart;
+            FloatPart = number.FloatPart;
+        }
     }
 }
