b6018a37659294e3103e097db341583b7a497c5c
razumovskiy <kolosovp94@gmail.com>
2020-10-17 11:00:40 +0300

    float converter implemented

---
+++ BitConverter.Core/Services/FromDecimal/FloatFromDecimal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using BitConverter.Auxiliaries;
+using BitConverter.Interfaces;
+
+namespace BitConverter.Services.FromDecimal
+{
+    public static class FloatFromDecimal
+    {
+        private static readonly StringBuilder Builder = new StringBuilder();
+        private const int Precision = 6;
+
+        public static string Convert(INumber number, int newBase)
+        {
+            if (newBase == 10)
+                return Math.Round(double.Parse(number.FloatPart)).ToString(CultureInfo.CurrentCulture);
+
+            var currentFloat = double.Parse("0," + number.FloatPart);
+
+            var queue = new Queue<char>();
+
+            for (var i = 0; i < Precision; i++)
+            {
+                currentFloat *= newBase;
+                var item = (int) currentFloat;
+                queue.Enqueue(BitTable.HexadecimalTable[item]);
+                currentFloat -= (int) currentFloat;
+            }
+
+            Builder.Clear();
+
+            while (queue.Any())
+                Builder.Append(queue.Dequeue());
+
+            return Builder.ToString();
+        }
+    }
+}
---
+++ BitConverter.Tests/ServiceTests/FromDecimal/FloatFromDecimalTest.cs
@@ -0,0 +1,20 @@
+﻿using BitConverter.Numbers;
+using BitConverter.Services.FromDecimal;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BitConverter.Tests.ServiceTests.FromDecimal
+{
+    [TestFixture]
+    public class FloatFromDecimalTest
+    {
+        [Test]
+        public void Float_From_Decimal_Convert_Test()
+        {
+            FloatFromDecimal.Convert(new DecimalNumber("0.372"), 2).Should().Be("010111");
+            FloatFromDecimal.Convert(new DecimalNumber("15.231"), 8).Should().Be("166213");
+            FloatFromDecimal.Convert(new DecimalNumber("15.231"), 16).Should().Be("3B22D0");
+            FloatFromDecimal.Convert(new DecimalNumber("15.231"), 2).Should().Be("001110");
+        }
+    }
+}
