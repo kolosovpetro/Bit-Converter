8e4324e25d81384ab98c8d0d84b9b84077c61686
razumovskiy <kolosovp94@gmail.com>
2020-10-17 11:35:57 +0300

    float converter tested

--- BitConverter.Tests/ServiceTests/FromDecimal/FloatFromDecimalTest.cs
+++ BitConverter.Tests/ServiceTests/FromDecimal/FloatFromDecimalTest.cs
@@ -15,6 +15,12 @@ namespace BitConverter.Tests.ServiceTests.FromDecimal
             FloatFromDecimal.Convert(new DecimalNumber("15.231"), 8).Should().Be("166213");
             FloatFromDecimal.Convert(new DecimalNumber("15.231"), 16).Should().Be("3B22D0");
             FloatFromDecimal.Convert(new DecimalNumber("15.231"), 2).Should().Be("001110");
+            FloatFromDecimal.Convert(new DecimalNumber("521.5"), 8).Should().Be("400000");
+            FloatFromDecimal.Convert(new DecimalNumber("521.5"), 16).Should().Be("800000");
+            FloatFromDecimal.Convert(new DecimalNumber("521.5"), 2).Should().Be("100000");
+            FloatFromDecimal.Convert(new DecimalNumber("83.05"), 8).Should().Be("031463");
+            FloatFromDecimal.Convert(new DecimalNumber("83.05"), 16).Should().Be("0CCCCC");
+            FloatFromDecimal.Convert(new DecimalNumber("83.05"), 2).Should().Be("000011");
         }
     }
 }
