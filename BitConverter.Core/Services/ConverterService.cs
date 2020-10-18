b4567a43997965cd6b8cc79c39eaf14b819d34fb
razumovskiy <kolosovp94@gmail.com>
2020-10-18 19:41:41 +0300

    converter service is modified

--- BitConverter.Core/Services/ConverterService.cs
+++ BitConverter.Core/Services/ConverterService.cs
@@ -1,5 +1,6 @@
 ﻿using BitConverter.Converters;
 using BitConverter.Interfaces;
+using BitConverter.Models;
 using BitConverter.Services.FromDecimal;
 using BitConverter.Validator.Auxiliaries;
 
@@ -17,6 +18,27 @@ namespace BitConverter.Services
             var integerPartFromDecimal = IntegerPartFromDecimal.Convert(decimalNumber, newBase);
             var floatPartFromDecimal = FloatPartFromDecimal.Convert(decimalNumber, newBase);
             return integerPartFromDecimal + Separator.Dot + floatPartFromDecimal;
+        }
+
+        private static INumber ConvertToDecimal(INumber number)
+        {
+            var decimalConverter = new DecimalConverter(number);
+            return decimalConverter.Convert();
+        }
+
+        private static INumber ConvertFromDecimal(INumber number, int newBase)
+        {
+            var decimalNumber = ConvertToDecimal(number);
+            var integerPartFromDecimal = IntegerPartFromDecimal.Convert(decimalNumber, newBase);
+            var floatPartFromDecimal = FloatPartFromDecimal.Convert(decimalNumber, newBase);
+            var data = integerPartFromDecimal + Separator.Dot + floatPartFromDecimal;
+            return new NumberModel(data, newBase);
+        }
+
+        public static INumber ConvertToBase(INumber number, int newBase)
+        {
+            var decimalNumber = ConvertToDecimal(number);
+            return ConvertFromDecimal(decimalNumber, newBase);
         }
     }
 }
